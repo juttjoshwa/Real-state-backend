bc7d36c13e1f4f3d6ed9dc24503b9a9c080920a9
juttjoshwa <juttjoshwa@example.com>
2023-12-11 09:48:11 +0500

    new

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import fileUpload from "express-fileupload";
-import dotenv from "dotenv"
-import cors from "cors"
+import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import DB_connect from "./DataBase/DB.js";
 
@@ -25,25 +25,21 @@ app.use(
 );
 app.use(cookieParser());
 
-
 app.get("/", (req, res) => {
-    res.status(200).send("server is working fine");
-  });
-
+  res.status(200).send("server is working fine");
+});
 
 // ! use routes at this point
 
-
-
-
-  
 app.listen(process.env.PORT, () => {
-    try {
-      console.log("server is working fine on http://localhost:4000/");
-    } catch (error) {
-      console.log(error.message);
-      process.exit(1);
-    }
-  });
+  try {
+    console.log("server is working fine on http://localhost:4000/");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+});
 
-  DB_connect()
+DB_connect();
+
+console.log("hel;llo");
