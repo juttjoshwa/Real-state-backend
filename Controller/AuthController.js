cd9efa5c84ef83abb35bdd7f556337f0616ed17a
juttjoshwa <juttjoshwa@example.com>
2023-12-28 07:34:42 +0500

    signIn

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,5 +1,6 @@
 import AuthModel from "../Models/AuthModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const SignUp = async (req, res) => {
   try {
@@ -26,6 +27,45 @@ export const SignUp = async (req, res) => {
       success: true,
       message: user,
     });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message || "something went wrong",
+    });
+  }
+};
+
+const secretKey = "juttjoshwa";
+
+export const SignIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await AuthModel.findOne({ email });
+    if (!validUser) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+    const ValidPassword = bcrypt.compareSync(password, validUser.password);
+    if (!ValidPassword) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid cridentials!",
+      });
+    }
+    const token = jwt.sign({ id: validUser._id }, secretKey);
+    const { password: pass, ...restof } = validUser._doc;
+
+    res
+      .cookie("tokken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({
+        success: true,
+        restof,
+      });
   } catch (error) {
     return res.status(500).json({
       success: false,
--- Router/AuthRouter.js
+++ Router/AuthRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { SignUp } from "../Controller/AuthController.js";
+import { SignIn, SignUp } from "../Controller/AuthController.js";
 
 const Router = express.Router();
 
 Router.post("/signup", SignUp);
+Router.post("/signin", SignIn);
 
 export default Router;
